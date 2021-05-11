28dbc9ac3b2c00a0b5bfa3fc474931bb301a5a1e
zhangbo2008 <15122306087@163.com>
2021-05-11 17:38:43 +0800

    no_comment

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+
+
+using Excel = Microsoft.Office.Interop.Excel;
+namespace Microsoft.Office.Interop
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel.Application xlApp;
+     
+            Excel.Workbook xlWorkBook;
+          //  xlApp = new Excel.Application();
+          xlApp= (Excel.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Excel.Application");
+            xlApp.Visible = true;
+            xlApp.ActiveWorkbook.Close();
+           
+     /*       xlWorkBook = xlApp.Workbooks.Open("c:/1.xlsx");
+            xlWorkBook.Close();*/
+
+            /*    Excel._Application objExcel;
+
+                objExcel = (Excel.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Excel.Application");
+                objExcel.ActiveWorkbook.Save();
+                int a = 3333;
+                //  objExcel.Workbooks.Open("c://1.xlsx");
+                MyExcel.Application EXC1 = new Excel.Application();*/
+
+
+            // System.Console.Write("dsafdsfsad");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
